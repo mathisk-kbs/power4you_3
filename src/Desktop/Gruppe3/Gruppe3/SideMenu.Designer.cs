6b45791157061474f52b07cc9ad567753e252069
Nils Lammers <lamnil3007@AD.GBS-GRAFSCHAFT.LOGODIDACT.NET>
2024-11-25 10:09:49 +0100

    added captions to side menu buttons, removed unneccesary buttons

--- src/Desktop/Gruppe3/Gruppe3/SideMenu.Designer.cs
+++ src/Desktop/Gruppe3/Gruppe3/SideMenu.Designer.cs
@@ -33,13 +33,12 @@ namespace Gruppe3
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.button1.Location = new System.Drawing.Point(0, 502);
+            this.button1.Location = new System.Drawing.Point(0, 455);
             this.button1.Margin = new System.Windows.Forms.Padding(0);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(150, 23);
@@ -56,7 +55,7 @@ namespace Gruppe3
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(150, 23);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "Kundenstamm";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -68,7 +67,7 @@ namespace Gruppe3
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(150, 23);
             this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
+            this.button3.Text = "Kunde anlegen";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // button4
@@ -79,31 +78,19 @@ namespace Gruppe3
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(150, 23);
             this.button4.TabIndex = 3;
-            this.button4.Text = "button4";
+            this.button4.Text = "Solarpanel";
             this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Dock = System.Windows.Forms.DockStyle.Top;
-            this.button5.Location = new System.Drawing.Point(0, 69);
-            this.button5.Margin = new System.Windows.Forms.Padding(0);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(150, 23);
-            this.button5.TabIndex = 4;
-            this.button5.Text = "button5";
-            this.button5.UseVisualStyleBackColor = true;
             // 
             // SideMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "SideMenu";
-            this.Size = new System.Drawing.Size(150, 525);
+            this.Size = new System.Drawing.Size(150, 478);
             this.ResumeLayout(false);
 
         }
@@ -114,6 +101,5 @@ namespace Gruppe3
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
     }
 }
